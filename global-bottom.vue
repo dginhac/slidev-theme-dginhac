c46e610efe91bcc110b612ad169ffee72eb1b365
d0m G <dginhac@ube.fr>
2026-06-01 11:16:34 +0200

    feat: add global-bottom slide counter component

---
+++ global-bottom.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="fixed bottom-2 right-2 z-50 px-2 py-1 rounded-full text-xs text-brand-muted bg-brand-faint border border-brand-border tabular-nums"
+        v-if="
+            $slidev.nav.currentLayout !== 'cover' &&
+            $slidev.nav.currentLayout !== 'end' &&
+            $slidev.nav.currentPage != 1
+        "
+    >
+        {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+    </div>
+</template>
